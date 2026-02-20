3ea377b82b866ffb9e62a1cdeebd3fb2687bf1f8
Merrick Liu <merrickliu168@gmail.com>
2026-02-20 12:15:47 -0500

    fix main

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -39,7 +39,10 @@ resource "aws_cloudwatch_log_group" "worker" {
 data "aws_iam_policy_document" "ecs_task_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service", identifiers = ["ecs-tasks.amazonaws.com"] }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
 
