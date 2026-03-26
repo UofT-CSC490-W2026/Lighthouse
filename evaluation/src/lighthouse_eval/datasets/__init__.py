bb06c10f6fd4c922df6758cc5a38fa535572d319
Rhys Balevicius <development@rhys.tech>
2026-03-26 19:10:11 -0400

    feat(eval): add dataset schema, YAML loader, and datasets package

---
+++ evaluation/src/lighthouse_eval/datasets/__init__.py
@@ -0,0 +1,27 @@
+from .schema import (
+    Dataset,
+    EvalResult,
+    EvaluatorKind,
+    MatchMetrics,
+    MatchSpec,
+    RetrievalMetrics,
+    RetrievalSpec,
+    Task,
+    TaskProvenance,
+    TestExecutionMetrics,
+    TestSpec,
+)
+
+__all__ = [
+    "Dataset",
+    "EvalResult",
+    "EvaluatorKind",
+    "MatchMetrics",
+    "MatchSpec",
+    "RetrievalMetrics",
+    "RetrievalSpec",
+    "Task",
+    "TaskProvenance",
+    "TestExecutionMetrics",
+    "TestSpec",
+]
