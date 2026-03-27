392fd358732857b4ac5ebae90bfefbb8446bc446
Rhys Balevicius <development@rhys.tech>
2026-03-26 23:44:19 -0400

    feat(embedding): add provider registry for selectable backends

--- packages/embedding/src/embedding/__init__.py
+++ packages/embedding/src/embedding/__init__.py
@@ -1,4 +1,13 @@
 from .base_provider import EmbeddingProvider
+from .bedrock_provider import BedrockEmbeddingProvider
 from .openai_provider import OpenAIEmbeddingProvider
+from .registry import EmbeddingStrategy, get_embedding_provider, register_embedding_provider
 
-__all__ = ["EmbeddingProvider", "OpenAIEmbeddingProvider"]
+__all__ = [
+    "BedrockEmbeddingProvider",
+    "EmbeddingProvider",
+    "EmbeddingStrategy",
+    "get_embedding_provider",
+    "OpenAIEmbeddingProvider",
+    "register_embedding_provider",
+]
