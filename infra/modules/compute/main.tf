9bce25de380c4d5d6d7ecf6b628f110bda658b9b
derekdhuynh <derek.5922.dh@gmail.com>
2026-03-30 14:22:39 -0400

    add ssm agend daemon

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -662,6 +662,8 @@ locals {
 
     dnf update -y
     dnf install -y docker docker-compose-plugin
+    systemctl enable amazon-ssm-agent
+    systemctl start amazon-ssm-agent
     systemctl enable docker
     systemctl start docker
 
